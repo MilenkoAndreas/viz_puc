a01604c705c40a5f1aae23af3280a443601290f4
Milenko Brzovic Cabrera <milenko@Milenkos-MacBook-Pro.local>
2022-07-30 14:08:45 -0400

    pages

--- home_page.py
+++ home_page.py
@@ -6,5 +6,30 @@ import streamlit as st
 import plotly.express as px
 import streamlit.components.v1 as components
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+#st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
 st.title("How many animals are slaughtered per year?",)
 st.text("The data is part of the FAOSTAT database from the Food and Agriculture Organization (FAO) of the United Nations. We have the yearly comsumption of meat from 1961 to 2020 ")
+
+sheet_id = st.secrets["sheet_id"]
+sheet_name = st.secrets["sheet_name"]
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+df=pd.read_csv(url)
+
+lst = ['Meat, chicken', 'Meat, cattle','Meat, pig','Meat, sheep']
+
+#components.html("<html><body><h1 style='color:white;font-size:10px'>The data is part of the FAOSTAT database from the Food and Agriculture Organization (FAO) of the United Nations. We have the yearly comsumption of meat from 1961 to 2020 </h1></body></html>")
+#components.html("<html><body><a href='https://www.fao.org'>FAO</a></body></html>")
+
+
+source=df.query('Area == "Chile"')\
+.query('Item in @lst ')
+source.Year = pd.to_datetime(source.Year, format='%Y')
