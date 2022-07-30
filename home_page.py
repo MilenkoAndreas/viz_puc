e7edcd6cc64f2171d41cf7f5fcc52b65eae4ecef
Milenko Brzovic Cabrera <milenko@Milenkos-MacBook-Pro.local>
2022-07-30 13:19:37 -0400

    new chapter structure

---
+++ home_page.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+#import altair as alt
+import streamlit as st
+#import plotly.figure_factory as ff
+import plotly.express as px
+import streamlit.components.v1 as components
+
+st.title("How many animals are slaughtered per year?",)
+st.text("The data is part of the FAOSTAT database from the Food and Agriculture Organization (FAO) of the United Nations. We have the yearly comsumption of meat from 1961 to 2020 ")
